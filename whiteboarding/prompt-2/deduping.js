1728680cfef1047e713f8c82bbd699d4b5f27614
bendunhampdx <bendunhampdx@gmail.com>
2021-11-12 10:51:52 -0800

    update second problem

---
+++ whiteboarding/prompt-2/deduping.js
@@ -0,0 +1,10 @@
+function deduping(arr) {
+  let newArr = [];
+  arr.forEach(function(item) {
+    if (!newArr.includes(item)) {
+      newArr.push(item);
+    };
+  });
+    
+  return newArr;
+  };
