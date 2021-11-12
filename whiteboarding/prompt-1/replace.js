fe3908ccb07822998a5d1f1db7b12053404ff1f9
bendunhampdx <bendunhampdx@gmail.com>
2021-11-12 10:26:57 -0800

    solve first problem

--- whiteboarding/prompt-1/replace.js
+++ whiteboarding/prompt-1/replace.js
@@ -1,3 +1,12 @@
 function replace(string) {
-  if (string .includes(' '))
+  let text = string.trim();
+  let newText = '';
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === ' ') {
+      newText += '%20';
+    } else {
+      newText += text[i];
+    }
+  }
+  return newText;
 }
